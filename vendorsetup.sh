7730f707ca901c9d49243e3b72a718dd359bbc07
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2022-11-28 14:46:26 +0100

    vili: Set-up firmware including
    * This will make vili ROM zips to ship prebuilt firmware, making things easier for all of us
    * [0mar99]: Git LFS is screwing with us, so let's just get the firmware from a separate repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -25,6 +25,10 @@ git clone https://github.com/AOSP-for-vili/vendor_xiaomi_sm8350-common.git -b li
 echo 'Cloning meme camera'
 git clone https://github.com/AOSP-for-vili/vendor_xiaomi_camera.git -b test vendor/xiaomi/camera
 
+# Firmware
+echo 'Cloning firmware'
+git clone https://gitlab.com/0mar99/vendor-xiaomi-vili-firmware.git vendor/xiaomi/vili-firmware
+
 # Leica patch
 echo 'Adding Leica camera patch'
 cd frameworks/base
