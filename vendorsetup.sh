089374e81024f414e14edcc8fa6d3368764bbf07
0mar99 <omarantonio9972@gmail.com>
2023-12-29 23:13:36 -0600

    vili: Add vendorsetup.sh
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,28 @@
+echo 'Cloning stuff needed to build for vili'
+
+# Device common
+echo 'Cloning common device tree'
+git clone https://github.com/AOSP-for-vili/device_xiaomi_sm8350-common.git -b pixel-wip device/xiaomi/sm8350-common
+
+# Kernel
+echo 'Cloning kernel'
+git clone https://github.com/AOSP-for-vili/android_kernel_xiaomi_sm8350.git --recursive -b lineage-22.1-ksunext kernel/xiaomi/sm8350
+
+# Hardware Xiaomi
+echo 'Cloning hardware xiaomi'
+rm -rf hardware/xiaomi
+git clone https://github.com/AOSP-for-vili/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
+
+# Vendor
+echo 'Cloning vendor tree'
+git clone https://github.com/AOSP-for-vili/vendor_xiaomi_vili.git -b lineage-22.2-wip vendor/xiaomi/vili
+
+# Vendor common
+echo 'Cloning common vendor tree'
+git clone https://github.com/AOSP-for-vili/vendor_xiaomi_sm8350-common.git -b lineage-22.2-wip vendor/xiaomi/sm8350-common
+
+# Meme camera
+echo 'Cloning meme camera'
+git clone https://github.com/AOSP-for-vili/vendor_xiaomi_camera.git -b test vendor/xiaomi/camera
+
+echo 'delete vendorsetup.sh from device tree once this is done'
