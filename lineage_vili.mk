833f56fff26ba7f366684a4f7968cd87fae3125a
LuK1337 <priv.luk@gmail.com>
2024-11-17 02:13:29 -0600

    vili: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_vili.mk
+++ lineage_vili.mk
@@ -14,20 +14,15 @@ $(call inherit-product, device/xiaomi/vili/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := vili
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2107113SG
 PRODUCT_NAME := lineage_vili
 
-PRODUCT_SYSTEM_NAME := vili_global
-PRODUCT_SYSTEM_DEVICE := vili
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vili_global-user 14 UKQ1.231207.002 V816.0.7.0.UKDMIXM release-keys" \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-BUILD_FINGERPRINT := Xiaomi/vili_global/vili:14/UKQ1.231207.002/V816.0.7.0.UKDMIXM:user/release-keys
+    BuildDesc="vili_global-user 14 UKQ1.231207.002 V816.0.7.0.UKDMIXM release-keys" \
+    BuildFingerprint=Xiaomi/vili_global/vili:14/UKQ1.231207.002/V816.0.7.0.UKDMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
