7c8a3b37e4e700057eac3468e9edd1d4019630d0
hassankhan2608 <hassankhan2608@gmail.com>
2025-04-07 16:46:37 +0530

    getting started

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,11 +2,15 @@ echo 'Cloning stuff needed to build for vili'
 
 # Device common
 echo 'Cloning common device tree'
-git clone https://github.com/AOSP-for-vili/device_xiaomi_sm8350-common.git -b lineage-22.1 device/xiaomi/sm8350-common
+git clone https://github.com/hassankhan2608/device_xiaomi_sm8350-common.git -b lineage-22.1 device/xiaomi/sm8350-common
 
 # Kernel
 echo 'Cloning kernel'
 git clone https://github.com/AOSP-for-vili/android_kernel_xiaomi_sm8350.git -b lineage-22.1 kernel/xiaomi/sm8350
+cd kernel/xiaomi/sm8350
+git submodule init
+git submodule update
+cd ../../../
 
 # Hardware Xiaomi
 echo 'Cloning hardware xiaomi'
@@ -15,24 +19,23 @@ git clone https://github.com/AOSP-for-vili/android_hardware_xiaomi.git -b lineag
 
 # Vendor
 echo 'Cloning vendor tree'
-git clone https://github.com/AOSP-for-vili/vendor_xiaomi_vili.git -b lineage-22.1 vendor/xiaomi/vili
+git clone https://github.com/hassankhan2608/vendor_xiaomi_vili.git -b lineage-22.1 vendor/xiaomi/vili
 
 # Vendor common
 echo 'Cloning common vendor tree'
-git clone https://github.com/AOSP-for-vili/vendor_xiaomi_sm8350-common.git -b lineage-22.1 vendor/xiaomi/sm8350-common
+git clone https://github.com/hassankhan2608/vendor_xiaomi_sm8350-common.git -b lineage-22.1 vendor/xiaomi/sm8350-common
 
 # Meme camera
 echo 'Cloning meme camera'
-git clone https://github.com/AOSP-for-vili/vendor_xiaomi_camera.git vendor/xiaomi/camera
+git clone https://github.com/AOSP-for-vili/vendor_xiaomi_camera.git -b test vendor/xiaomi/camera
 
 # Firmware
 echo 'Cloning firmware'
 git clone https://gitlab.com/0mar99/vendor-xiaomi-vili-firmware.git vendor/xiaomi/vili-firmware
 
 # Leica patch
-echo 'Adding Leica camera patch'
 cd frameworks/base
-wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/14/Leicamera/0001-Add-backwards-compatible-CaptureResultExtras-constructor.patch
+get https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/14/Leicamera/0001-Add-backwards-compatible-CaptureResultExtras-constructor.patch
 patch -p1 <0001-Add-backwards-compatible-CaptureResultExtras-constructor.patch
 cd ../..
 
